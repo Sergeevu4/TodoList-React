77e93ed9be1be24e7a6bceb9a85234750abf3975
Sergeevu4 <sergei.sergeevu4@gmail.com>
2020-03-27 14:44:23 +0300

    Updates

---
+++ precache-manifest.52064b9d39da5d7bf11c9755a2cefde7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8a18991855763bbfe943191c03137ae",
+    "url": "/TodoList-React/index.html"
+  },
+  {
+    "revision": "07a8c56181415f61cc46",
+    "url": "/TodoList-React/static/css/main.b0f9ac64.chunk.css"
+  },
+  {
+    "revision": "532a3c6b7a2b83751e6b",
+    "url": "/TodoList-React/static/js/2.8e9e2458.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/TodoList-React/static/js/2.8e9e2458.chunk.js.LICENSE"
+  },
+  {
+    "revision": "07a8c56181415f61cc46",
+    "url": "/TodoList-React/static/js/main.d900b1ce.chunk.js"
+  },
+  {
+    "revision": "b92eb9e4511d86364c0c",
+    "url": "/TodoList-React/static/js/runtime-main.c9c4ce2b.js"
+  }
+]);
